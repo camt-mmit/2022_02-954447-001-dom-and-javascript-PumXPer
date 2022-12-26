5b367528ef108f06180cc3738a7e30efdc793ede
Phornpitak Kadta <86459508+PumXPer@users.noreply.github.com>
2022-12-26 15:12:24 +0700

    week2

--- src/js/dom-05-module.js
+++ src/js/dom-05-module.js
@@ -1,37 +1,8 @@
 import { assign as assignInput } from './input.js';
 
-function AddSection(master) {
+document.addEventListener('DOMContentLoaded', () => {
   const inputTemplate = document.querySelector('template#tmp-input');
   const inputSection = document.querySelector('.cmp-input-section');
-  const Data = document.querySelector('.data');
-
-  const section = document.createElement('section');
-  section.classList.add('cmp-input-sec');
-
-  section.append(Data);
-  master.append(section);
 
   assignInput(inputSection, inputTemplate);
-}
-
-function AssignSec(master) {
-  const outdata = master.querySelector('.output-data');
-
-  console.debug(master);
-
-  master.addEventListener('click', (rr) => {
-    if (rr.target.matches('.cmd-add-section')) {
-      AddSection(outdata, master);
-    }
-  });
-
-  AddSection(outdata, master);
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  const master = document.querySelector('.master');
-
-  console.debug(master);
-
-  AssignSec(master);
 });
